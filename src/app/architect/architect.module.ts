fc1983a3094dc50e3792f5036c4bfbd8612bd488
Artur Jarosz <azjarosz@gmail.com>
2023-09-18 23:42:51 +0200

    TA-360 Add Architect detail page

--- src/app/architect/architect.module.ts
+++ src/app/architect/architect.module.ts
@@ -6,19 +6,33 @@ import {RippleModule} from "primeng/ripple";
 import {TableModule} from "primeng/table";
 import {ArchitectRestService, ArchitectRestServiceImpl} from "./service/architect-rest.service";
 import {SharedModule} from "../shared/shared.module";
+import { ArchitectDetailComponent } from './architect-detail/architect-detail.component';
+import {DialogModule} from "primeng/dialog";
+import {DropdownModule} from "primeng/dropdown";
+import {InputTextModule} from "primeng/inputtext";
+import {InputTextareaModule} from "primeng/inputtextarea";
+import {NgIf, NgSwitchCase} from "@angular/common";
 
 @NgModule({
     declarations: [
-        ArchitectListComponent
+        ArchitectListComponent,
+        ArchitectDetailComponent
     ],
     imports: [
         RouterModule.forChild([
-            {path: 'architects', component: ArchitectListComponent}
+            {path: 'architects', component: ArchitectListComponent},
+            {path: 'architects/:id', component: ArchitectDetailComponent}
         ]),
         ButtonModule,
         RippleModule,
         SharedModule,
         TableModule,
+        DialogModule,
+        DropdownModule,
+        InputTextModule,
+        InputTextareaModule,
+        NgIf,
+        NgSwitchCase,
     ],
     providers: [
         {provide: ArchitectRestService, useClass: ArchitectRestServiceImpl}
