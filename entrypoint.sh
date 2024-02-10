30810996b539fde1b5143726921c43a86eaba10c
Artur Jarosz <45771947+ArturJarosz@users.noreply.github.com>
2024-02-10 20:00:22 +0100

    TA-412 Change version of base nginx image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
-#! /bin/sh
-
-set -e
-
-echo PORT=[$PORT]
-echo BASE_URL=[$BASE_URL]
+echo PORT=$PORT
+echo BASE_URL=$BASE_URL
 envsubst '$PORT,$BASE_URL' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 sed -i "s~BASE_URL~$BASE_URL~g" /usr/share/nginx/html/main.*.js
